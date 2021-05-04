c325102614eb73b63a4b036fe55ee3d67064bc9d
MalcolmBrundin <Malcolm.Brundin.7672@student.uu.se>
2021-05-04 11:04:17 +0200

    Fixade sökfunktionen. Kvar att göra: Sista bokstaven är liten. Koppla till kartan.

--- js/data-search.js
+++ js/data-search.js
@@ -1,28 +1,26 @@
+
+
 function searchInput() {
   let input = document.getElementById('myInput');
-  let selectedInformation = input.value;
+  let selectedInformation = input.value.toUpperCase();
+  let information = document.getElementById('information');
+  information.style.display = "none";
   try {
     NATIONAL_INTERESTS.forEach(element => {
       if (String(selectedInformation) == String(element.id)) {
         selectedInformation = element;
+        information.style.display = "block";
       }
     });
-    showSearchedInput(selectedInformation);
+      showSearchedInput(selectedInformation);
   } catch (error) {
     console.log('ERROR: Search error');
   }
 }
 
 function showSearchedInput(element) {
-  
-  console.log(element.namn);
   let information = document.getElementById('information');
-  if(element != null) {
-    information.innerHTML = `<h2 id="name">${element.namn}</h2>
-    <p id="id"><b>${element.id}</b></p>
-    <p id="description">${element.motivering, element.uttryck}</p>`
-  } else {
-    information.style.display = 'none';
-  }
-
+  information.innerHTML = `<h2 id="name">${element.namn}</h2>
+  <p id="id"><b>${element.id}</b></p>
+  <p id="description">${element.motivering, element.uttryck}</p>`;
 }
