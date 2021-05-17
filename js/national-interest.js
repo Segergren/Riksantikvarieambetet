d3a33c8adf8e7d137c9ebfae981808fb781706db
Olle Segergren <olle.segergren@gmail.com>
2021-05-17 10:46:56 +0200

    laggt till Ingen information finns tillgänglig

--- js/national-interest.js
+++ js/national-interest.js
@@ -2,13 +2,13 @@ class NationalInterest {
     constructor(element) {
         this.name = element.Namn_RI;
         this.id = element.RI_ID;
-        this.reason = element.hasOwnProperty('motivering') ? element.motivering : "<i>Inget information finns tillgänglig</i>";
-        this.expression = element.hasOwnProperty('Uttryck_för_RI') ? element.Uttryck_för_RI : "<i>Inget information finns tillgänglig</i>";
+        this.reason = element.hasOwnProperty('motivering') ? element.motivering : "<i>Ingen information finns tillgänglig</i>";
+        this.expression = element.hasOwnProperty('Uttryck_för_RI') ? element.Uttryck_för_RI : "<i>Ingen information finns tillgänglig</i>";
         this.culturalEnvironmentTypes = element.hasOwnProperty('Kulturmiljötyper kursiverade i text') ? element["Kulturmiljötyper kursiverade i text"] : "<i>Inget information finns tillgänglig</i>";
         this.culturalEnvironmentTypesParenthes = element.hasOwnProperty('Kulturmiljötyper inom prantes') ? element["Kulturmiljötyper inom prantes"] : false;
         this.county = element.hasOwnProperty('Län') ? element.Län : false;
         this.municipality = element.hasOwnProperty('Kn') ? element.Kn : false;
-        this.firstRevision  = element.hasOwnProperty('Tidigare revidering RAÄ') ? element["Tidigare revidering RAÄ"] : "<i>Inget information finns tillgänglig</i>";
+        this.firstRevision  = element.hasOwnProperty('Tidigare revidering RAÄ') ? element["Tidigare revidering RAÄ"] : "<i>Ingen information finns tillgänglig</i>";
         this.latestRevision = element.hasOwnProperty('Senast_reviderad') ? element.Senast_reviderad : false;
         if(element.hasOwnProperty('Utredningsområde')){
             this.underInvestigation = (element.Utredningsområde.includes("x")) ? true : false;
